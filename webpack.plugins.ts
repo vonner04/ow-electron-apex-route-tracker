cdf413700764e588d403636e213011d96c9f457c
vonner04 <randyleean092m@gmail.com>
2025-10-30 01:32:20 +1300

    bugFix: failed to resolve index.html file. HtmlWebpackPlugin generates index.html for the dev server.

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,4 +1,6 @@
 import type IForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
@@ -6,5 +8,9 @@ const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require("
 export const plugins = [
   new ForkTsCheckerWebpackPlugin({
     logger: "webpack-infrastructure",
+  }),
+  new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, "src/renderer/index.html"),
+    filename: "index.html",
   }),
 ];
